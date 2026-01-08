9675ed54f4daac71e73091a7c730da1e26bef075
Amrit Podder <design2code93@gmail.com>
2026-01-08 15:39:36 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    // logic: Look in the root folder (./) and any subfolders for .html or .js files
+    content: ["./**/*.{html,js}"], 
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
